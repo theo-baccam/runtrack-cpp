2837aa81736754d23023618f88b9b3fc831e93b1
theo-baccam <theo.baccam@laplateforme.io>
2024-08-29 16:07:24 +0200

    jour01/job09: utilisation pointeur

--- jour01/job09/job09.cpp
+++ jour01/job09/job09.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
-int getSmallestIntegerInArray(int integerArray[], int arraySize) {
-    int smallestInteger = integerArray[0];
+int* getSmallestIntegerPointer(int (&integerArray)[], int arraySize) {
+    int* smallestInteger = &integerArray[0];
     for (int i = 1; i < arraySize; i++) {
-        if (integerArray[i] < smallestInteger) {
-            smallestInteger = integerArray[i];
+        if (integerArray[i] < *smallestInteger) {
+            smallestInteger = &integerArray[i];
         };
     };
 
@@ -13,14 +13,14 @@ int getSmallestIntegerInArray(int integerArray[], int arraySize) {
 
 
 int main() {
-    int aSize = 6;
-    int a[aSize] = {92, 17412, 74, 1, 83, 2};
+    int arraySize = 6;
+    int array[arraySize] = {92, 17412, 74, 1, 83, 2};
 
-    int bSize = 4;
-    int b[bSize] = {182, -1932, 852, 123};
-
-    std::cout << "a: " << getSmallestIntegerInArray(a, aSize) << '\n';
-    std::cout << "b: " << getSmallestIntegerInArray(b, bSize) << '\n';
+    std::cout << "Adresse du plus petit entier: " << &array[3] << '\n';
+    std::cout
+        << "Adresse pointee par pointeur: "
+        << getSmallestIntegerPointer(array, arraySize)
+        << '\n';
 
     return 0;
 };
